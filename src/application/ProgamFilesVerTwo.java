eb37fa215f18cc3b4e02d8593eb09baad263ef6d
MatheusDev22 <matheusbsb95@gmail.com>
2022-01-12 14:01:38 -0300

    BlockTryWithResourcer

--- src/application/ProgamFilesVerTwo.java
+++ src/application/ProgamFilesVerTwo.java
@@ -8,14 +8,10 @@ public class ProgamFilesVerTwo {
 
 	public static void main (String[] args) {
 		
-		String path = "C:\\temp\\int.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
+		String path = "C:\\temp\\in.txt";
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path)) ){
+						
 			String line = br.readLine();
 			
 			while (line != null) {
@@ -26,18 +22,6 @@ public class ProgamFilesVerTwo {
 		catch(IOException e) {
 			System.out.println("Error: " +  e.getMessage() );
 		}
-		finally {
-			try {
-				if(br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-				
-			}catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
+		
 	}
 }
